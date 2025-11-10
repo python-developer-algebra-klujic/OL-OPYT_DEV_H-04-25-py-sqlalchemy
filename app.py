d9e8ab789230f820354cf574d50c423f8d33d53d
Python Developer Algebra Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2025-11-10 21:04:02 +0100

    SQLAlchemy finished

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from sqlalchemy.orm import declarative_base, relationship
-from sqlalchemy import Column, Integer, String, ForeignKey, create_engine
+from sqlalchemy.orm import declarative_base, relationship, Session
+from sqlalchemy import Column, Integer, String, ForeignKey, create_engine, or_, and_
 
 # 1. KORAK - Pripremiti strukturu baze i tablice
 
@@ -19,7 +19,7 @@ class Author(Base):
     books = relationship("Book", back_populates="author")
 
     def __repr__(self):
-        return f"Author: {self.first_name} {self.last_name}"
+        return f"ID: {self.id} | Author: {self.first_name} {self.last_name}"
 
 
 class Book(Base):
@@ -36,11 +36,94 @@ class Book(Base):
     author = relationship("Author", back_populates="books")
 
     def __repr__(self):
-        return f"Book: {self.title}"
+        return f"ID: {self.id} | Book: {self.title}"
 
 
 # 2. KORAK - Kreiranje konekcije na s bazom pomocu objekta 'engine'
+# engine je slicno kao i connection u sqlite3 modulu
 engine = create_engine("sqlite:///library.db")
 
 # 3. KORAK - Kreiranje baze i tablica na osnovu klasa koje naslijeduju Base klasu
 Base.metadata.create_all(engine)
+
+
+# 4. KORAK - Rad s podacima
+# Za to koristimo Session klasu, odnosno session objekt te klase.
+# session je slicno cusrosru u sqlite3 modulu
+try:
+    with Session(engine) as session:
+        # C - CREATE - dodaj podatke u bazu
+        # Dodan mehanizam zastite dodavanja duplih podataka!!!
+        tolkien = Author(first_name="J.R.R.", last_name="Tolkien")
+
+        the_hobit = Book(title="The Hobit", author=tolkien)
+        the_fellowship_of_the_ring = Book(title="The Fellowship of the Ring", author=tolkien)
+
+        author_from_db = session.query(Author).filter(
+            and_(Author.first_name == tolkien.first_name,
+                 Author.last_name == tolkien.last_name,)
+        ).first()
+        if author_from_db == None:
+            session.add(tolkien)    # kao da smo napravili cursor.execute()
+            session.commit()        # pohrani promjene u bazu
+        else:
+            print(f"Autor {tolkien.first_name} {tolkien.last_name} vec postoji u bazi")
+
+
+        # R - READ - dohvati podatke iz baze
+        # authors = session.query(Author).all()
+        # for author in authors:
+        #     print(author)
+
+        # author = session.query(Author).filter_by(id=3).first()
+        # print(author)
+
+        # author = session.query(Author).filter_by(last_name="Mihajlovic Dostojevski").first()
+        # print(author)
+
+        # author = session.query(Author).filter(Author.last_name.like("Tol%")).first()
+        # print(author)
+
+
+        # books = session.query(Book).all()
+        # for book in books:
+        #     print(book)
+
+        #       JOIN EKSPLICITNI - vracanje podataka iz obje baze
+        # resut = session.query(Book).join(Author).all()
+        # for book in resut:
+        #     print(book, " | ", book.author)
+
+        #       JOIN IMPLICITNI - vracanje podataka iz obje baze
+        # resut = session.query(Book).all()
+        # for book in resut:
+        #     print(book, " | ", book.author)
+
+
+        # U - UPDATE - azuriraj podatke iz baze
+        # ili oznaci kao izbrisan u slucaju ako se koristi Soft Delete
+        # author_from_db = session.query(Author).filter_by(id=4).first()
+        # if author_from_db:
+        #     author_from_db.first_name = "Jules"
+        #     author_from_db.last_name = "Verne"
+        #     session.commit()
+        # else:
+        #     print(f"Ne postoji trazeni autor u bazi!")
+
+        # D - DELETE - izbrisi objek iz base
+        # books_from_db = session.query(Book).filter(or_(Book.id==7, Book.id==8)).all()
+        # if len(books_from_db) > 0:
+        #     for book in books_from_db:
+        #         session.delete(book)
+
+        # author_from_db = session.query(Author).filter_by(id=4).first()
+        # if author_from_db:
+        #     session.delete(author_from_db)
+        #     session.commit()
+        # else:
+        #     print(f"Ne postoji trazeni autor u bazi!")
+
+
+
+except Exception as ex:
+    print(f"Dogodila se greska {ex}.")
